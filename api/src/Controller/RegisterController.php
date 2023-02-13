6961cc2573d498fa7969a07350e6d5e3e3ffe282
victor <valee.victor@gmail.com>
2023-02-13 21:26:59 +0100

    apiplatform

--- api/src/Controller/RegisterController.php
+++ api/src/Controller/RegisterController.php
@@ -6,21 +6,62 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 #[AsController]
 class RegisterController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private UserPasswordEncoderInterface $passwordEncoder,
         private RequestStack $request,
-
+        private UserPasswordHasherInterface $passwordHasher,
     ) {}
 
-    public function __invoke(Book $book): Book
+    public function __invoke()
     {
-        $this->bookPublishingHandler->handle($book);
+        dd("function __invoke");
+        $request = $this->requestStack->getCurrentRequest();
+        $em = $this->managerRegistry->getManager();
 
-        return $book;
+        $body = json_decode($request->getContent());
+
+        $email = $body->email;
+        $password = $body->password;
+        $role = "ROLE_USER";
+
+        $user = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $password
+        );
+        $user->setEmail($email);
+        $user->setRoles([$role]);
+
+        $user->setIsPasswordRequest(false);
+        $user->setPassword($hashedPassword);
+    }
+
+    public function signUpHandler(SignUpRequest $signUpRequest): JsonResponse
+    {
+        dd("function signUpHandler");
+        if(!$this->signUpValidator->validate($signUpRequest)){
+            return new JsonResponse([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'errors' => $this->signUpValidator->getErrors()
+            ]);
+        }
+
+        $user = $this->userCreator->createUser($signUpRequest);
+
+        return new JsonResponse([
+            'status' => Response::HTTP_OK,
+            'entity' => $user->getId()
+        ]);
     }
 }
